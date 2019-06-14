c94e7515fb34a6d1f9d75b96fb607d2aa5cf41bb
choleece <chaolizheng@sf-express.com>
2019-06-14 18:38:12 +0800

    linked list

--- collection/src/main/java/cn/choleece/base/collection/list/LinkedListTest.java
+++ collection/src/main/java/cn/choleece/base/collection/list/LinkedListTest.java
@@ -1,5 +1,6 @@
 package cn.choleece.base.collection.list;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -48,10 +49,101 @@ public class LinkedListTest {
 
         System.out.println("------list.add(int index, E e)------");
         list.forEach(s -> System.out.println(s));
+
+        list.addAll(new ArrayList<String>() {
+            {
+                add("chao");
+                add("li");
+            }
+        });
+
+        /**
+         * 底层实现是针对c进行遍历插入，其实是一种特殊的list.addAll(index, collection)，此时index为size
+         */
+        System.out.println("------list.addAll(Collection c)------");
+        list.forEach(s -> System.out.println(s));
+
+        list.addAll(1, new ArrayList<String>() {
+            {
+                add("y");
+                add("t");
+            }
+        });
+
+        /**
+         * 底层实现是,在指定的位置index,针对c进行遍历插入
+         */
+        System.out.println("------list.addAll(int index, Collection c)------");
+        for (String s : list) {
+            System.out.println(s);
+        }
+        list.forEach(s -> System.out.println(s));
+    }
+
+    public static void testGetAndSet() {
+        List<String> list = new LinkedList<>();
+        list.add("bing");
+
+        System.out.println("------init------");
+        list.forEach(s -> System.out.println(s));
+
+        System.out.println("------get------");
+        /**
+         * get操作，会先找到index对应的内容，然后返回，找到index跟上边添加add(int index, Node n)逻辑一直
+         */
+        System.out.println(list.get(0));
+
+        /**
+         * set操作，与get同理，只是会替换内容
+         */
+        list.set(0, "choleece");
+        System.out.println("------set------");
+        list.forEach(s -> System.out.println(s));
+
+    }
+
+    public static void testRemove() {
+        List<String> list = new LinkedList<>();
+        list.add("bing");
+        list.add("choleece");
+        list.add("zheng");
+        list.add("chao");
+
+        System.out.println("------init------");
+        System.out.println(list);
+
+        /**
+         * 都是找到对应index的node，然后对其进行unlink操作
+         */
+        list.remove(0);
+
+        System.out.println("------remove(int index)------");
+        System.out.println(list);
+
+        /**
+         * 根据值找到对应node，然后对其进行unlink操作
+         */
+        list.remove("bing");
+
+        System.out.println("------remove(Object o)------");
+        System.out.println(list);
+
+        /**
+         * 与remove(Object o)同理，对collection进行遍历,循环调用remove
+         */
+        list.removeAll(new ArrayList<String>(){{
+            add("zheng");
+        }});
+
+        System.out.println("------removeAll(Collection c)------");
+        System.out.println(list);
+
     }
 
     public static void main(String[] args) {
-        testAdd();
+//        testAdd();
+//        testGetAndSet();
+        testRemove();
     }
 
 }
