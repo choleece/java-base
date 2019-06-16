129a3f4af2395691872fc263b8151631ba29f9df
choleece <choleece@163.com>
2019-06-16 22:41:19 +0800

    LinkedHashSet

--- collection/src/main/java/cn/choleece/base/collection/map/LinkedHashMapTest.java
+++ collection/src/main/java/cn/choleece/base/collection/map/LinkedHashMapTest.java
@@ -5,15 +5,26 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * LinkedHashMap是一个HashMap加List(底层是个双链表)的实现，其拥有的特性跟HashMap一样，存储结构依旧也没变，只是把每个节点的关系都维护在一个双向链表上
+ * LinkedHashMap是一个HashMap加List(底层是个双链表)的实现，其拥有的特性跟HashMap一样，存储结构依旧也没变，只是把每个节点的关系都维护在一个双向链表上，其能保持一个插入顺序
  * Created by choleece on 2019/6/16.
  */
 public class LinkedHashMapTest {
 
     /**
      * 不论哪种构造函数，其底层都是调用了HashMap的构造函数
+     * /**
+     * HashMap.Node subclass for normal LinkedHashMap entries.
+     *
+     * LinkedHashMap维护链关系的结构，比HashMap的Node多了一个向前、向后的指针
+     *static class Entry<K,V> extends HashMap.Node<K,V> {
+     *  LinkedHashMap.Entry<K,V> before, after;
+     *  Entry(int hash, K key, V value, HashMap.Node<K,V> next) {
+     *       super(hash, key, value, next);
+     *  }
+     *}
      */
     public static void testConstructLinkedHashMap() {
+
         /**
          * 无参构造函数
          */
@@ -38,6 +49,7 @@ public class LinkedHashMapTest {
     public static void main(String[] args) {
         Map<String, String> map = new LinkedHashMap<>();
         map.put("100", "choleece");
+        map.put("102", "choleece");
         map.put("101", "bing");
 
         System.out.println("----------entry map----------");
--- collection/src/main/java/cn/choleece/base/collection/set/LinkedHashSetTest.java
+++ collection/src/main/java/cn/choleece/base/collection/set/LinkedHashSetTest.java
@@ -1,5 +1,7 @@
 package cn.choleece.base.collection.set;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -11,8 +13,49 @@ import java.util.Set;
  */
 public class LinkedHashSetTest {
 
+    /**
+     * 调用的是继承HashSet的构造函数，HashSet的构造函数有个参数，叫dummy，设置为true，表示构造一个LinkedHashMap实例
+     */
+    public static void testConstructLinkedHashSet() {
+
+        /**
+         * 无参构造函数，
+         */
+        new LinkedHashSet<>();
+
+        /**
+         * 指定Capacity的构造函数
+         */
+        new LinkedHashSet<>(8);
+
+        /**
+         * 指定Capacity & LoadFactor的构造函数
+         */
+        new LinkedHashSet<>(8, 1);
+
+        /**
+         * 指定初始化集合的构造函数
+         */
+        new LinkedHashSet<String>(new HashSet<String>(){{add("choleece");}});
+
+
+    }
+
     public static void main(String[] args) {
         Set<String> set = new LinkedHashSet<String>();
+
+        set.add("One");
+        set.add("Two");
+        set.add("Three");
+        set.add("Four");
+        set.add("Five");
+
+        set.add("Five");
+
+        Iterator<String> i = set.iterator();
+        while(i.hasNext()) {
+            System.out.println(i.next());
+        }
     }
 
 }
