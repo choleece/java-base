e5bed942d779b85b1893dfd3b4c864561e1551bb
choleece <choleece@163.com>
2019-09-05 23:40:27 +0800

    second solution to reverse linked list

--- leet-code/src/main/java/cn/choleece/base/leetcode/code206/Solution206.java
+++ leet-code/src/main/java/cn/choleece/base/leetcode/code206/Solution206.java
@@ -29,12 +29,34 @@ public class Solution206 {
         return newHead.next;
     }
 
+    public static ListNode reerseList1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode cur = head, tmpHead = head;
+
+        while (cur.next != null) {
+            ListNode tmp = cur.next;
+
+            cur.next = tmp.next;
+            tmp.next = tmpHead;
+            tmpHead = tmp;
+        }
+
+        return tmpHead;
+    }
+
     public static void main(String[] args) {
         ListNode head = ListNode.initList(Arrays.asList(1, 2, 3, 4, 5));
 
         ListNode.loopList(head);
 
         head = reverseList(head);
+
+        ListNode.loopList(head);
+
+        head = reerseList1(head);
 
         ListNode.loopList(head);
     }
