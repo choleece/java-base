b04a281abcc84c96267a5217643050c218266614
choleece <choleece@163.com>
2019-09-08 23:20:17 +0800

    tree data structure

--- data-structure/src/main/java/cn/choleece/base/structure/tree/TreeNode.java
+++ data-structure/src/main/java/cn/choleece/base/structure/tree/TreeNode.java
@@ -1,9 +1,6 @@
 package cn.choleece.base.structure.tree;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 树结构
@@ -85,7 +82,7 @@ public class TreeNode {
     }
 
     /**
-     * 层序遍历（借助队列先进先出的思想）（递归函数本身是一个栈）
+     * 层序遍历（借助队列先进先出的思想），本质是一种BFS
      */
     public static void levelOrder(TreeNode root) {
         if (root == null) {
@@ -105,6 +102,129 @@ public class TreeNode {
         }
     }
 
+    /**
+     * BSP代码模版（广度优先搜索）此处的TreeNode可以换成图等数据结构
+     * @param root
+     */
+    public static void bfs(TreeNode root) {
+        if (root == null)  {
+            return;
+        }
+
+        // 用队列存储将要访问的节点，因为队列有FIFO的特性，所以会保证按照广度的顺序进行
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        // 将待访问的节点放入到队列中去
+        queue.add(root);
+
+        // 存放已经访问过的节点，保证已经访问过的节点将不再继续访问
+        Set<TreeNode> visitedSet = new HashSet<TreeNode>();
+        visitedSet.add(root);
+
+        while (!queue.isEmpty()) {
+            // 取出队列头部的节点
+            TreeNode tmpNode = queue.poll();
+            // 将已访问过的节点放入访问队列中
+            visitedSet.add(tmpNode);
+
+            // 针对节点进行处理
+            precessNode(tmpNode);
+
+            // 获取节点待访问的所有节点
+            List<TreeNode> nodes = generateRelatedNodes(tmpNode);
+
+            // 将新的节点加入到队列中
+            queue.addAll(nodes);
+        }
+    }
+
+    /**
+     * DFS模版（深度优先搜索）此处的TreeNode可以换成图等数据结构, DFS只要是利用栈FILO的思想
+     * @param root
+     */
+    public static void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        // 用栈存储将要访问的节点，因为栈有FILO的特性，所以会保证按照深度的顺序进行
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.add(root);
+
+        // 存放已经访问过的节点，保证已经访问过的节点将不再继续访问
+        Set<TreeNode> visitedSet = new HashSet<TreeNode>();
+        visitedSet.add(root);
+
+        while (!stack.isEmpty()) {
+            // 取出队列头部的节点
+            TreeNode tmpNode = stack.pop();
+            // 将已访问过的节点放入访问栈中
+            visitedSet.add(tmpNode);
+
+            // 针对节点进行处理
+            precessNode(tmpNode);
+
+            // 获取节点待访问的所有节点
+            List<TreeNode> nodes = generateRelatedNodes(tmpNode);
+
+            // 将新的节点加入到栈中
+            for (TreeNode node : nodes) {
+                stack.push(node);
+            }
+        }
+
+    }
+
+    /**
+     * DFS 递归写法 （因为递归函数本身就是一个调用栈，所以可以不用一个单独的栈）
+     * @param root
+     * @param visitedSet 在树这个结构里可以不需要，因为树不指回原来的节点
+     */
+    public static void recursionDfs(TreeNode root, Set<TreeNode> visitedSet) {
+        if (root == null) {
+            return;
+        }
+        // 存放已经访问过的节点，保证已经访问过的节点将不再继续访问
+        visitedSet.add(root);
+
+        // 针对节点进行处理
+        precessNode(root);
+
+        // 获取节点待访问的所有节点
+        List<TreeNode> nodes = generateRelatedNodes(root);
+
+        for (TreeNode node : nodes) {
+            // 如果没有被访问过，那么可以继续访问
+            if (!visitedSet.contains(node)) {
+                recursionDfs(node, visitedSet);
+            }
+        }
+    }
+
+    /**
+     * 针对节点进行业务处理
+     * @param node
+     */
+    public static void precessNode(TreeNode node) {
+        System.out.println("node val: " + node.val);
+    }
+
+    /**
+     * 获取节点待访问的所有节点
+     * @param node
+     * @return
+     */
+    public static List<TreeNode> generateRelatedNodes(TreeNode node) {
+        List<TreeNode> list = new LinkedList<TreeNode>();
+        if (node.left != null) {
+            list.add(node.left);
+        }
+        if (node.right != null) {
+            list.add(node.right);
+        }
+
+        return list;
+    }
+
     public static void main(String[] args) {
 //        TreeNode root = new TreeNode(20, new TreeNode(10, new TreeNode(5), new TreeNode(15)), new TreeNode(30, new TreeNode(25), new TreeNode(35)));
 
@@ -121,5 +241,15 @@ public class TreeNode {
 
         System.out.println("levelOrder: ");
         levelOrder(root);
+
+        System.out.println("bfs tree: ");
+        bfs(root);
+
+        System.out.println("no recursion dfs tree: ");
+        dfs(root);
+
+        System.out.println("recursion dfs tree: ");
+        Set<TreeNode> visitedSet = new HashSet<TreeNode>();
+        recursionDfs(root, visitedSet);
     }
 }
