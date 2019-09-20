5a28c5db0135668efdcd196e1cd45354bbcb8cb8
choleece <chaolizheng@sf-express.com>
2019-09-20 10:11:03 +0800

    thead demo

--- collection/src/main/java/cn/choleece/base/thread/ThreadTest.java
+++ collection/src/main/java/cn/choleece/base/thread/ThreadTest.java
@@ -1,5 +1,8 @@
 package cn.choleece.base.thread;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ThreadTest {
 
     public static void main(String[] args) {
@@ -8,43 +11,49 @@ public class ThreadTest {
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("锁在类上" + ThreadDemo.getA());
+                System.out.println("thread-1" + threadDemo.getA());
             }
         });
 
         Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("锁在对象上" + ThreadDemo.getBWithOutLock());
+                System.out.println("thread-2" + threadDemo.getA());
             }
         });
 
         thread1.start();
+        thread2.start();
 
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        thread2.start();
     }
 
 }
 
 class ThreadDemo {
 
-    public static int a = 0;
+    public int a = 0;
 
-    public synchronized static int getA() {
+    Object syncObj = new Object();
+
+    public int getA() {
+        Lock lock = new ReentrantLock();
+        lock.lock();
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
         return a;
     }
 
-    public synchronized static int getBWithOutLock() {
+    public int getBWithOutLock() {
         return a;
     }
 }
