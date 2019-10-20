25c80ca5c5a853bae6f3b922bb4b9ddeb12f1c6f
choleece <choleece@163.com>
2019-10-20 17:08:33 +0800

    锁优化

--- data-structure/src/main/java/cn/choleece/base/structure/list/ListNode.java
+++ data-structure/src/main/java/cn/choleece/base/structure/list/ListNode.java
@@ -36,9 +36,66 @@ public class ListNode {
         return head;
     }
 
+    public static ListNode reverseList(ListNode root) {
+
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = root;
+        ListNode p = root;
+
+        while (p.next != null) {
+
+            ListNode tmp = p.next;
+            p.next = tmp.next;
+            tmp.next = dummyHead.next;
+
+            dummyHead.next = tmp;
+        }
+
+        return dummyHead.next;
+
+    }
+    public static ListNode segmentReverseList(ListNode root, int k) {
+
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = root;
+        ListNode p = root, tmpDummy = dummyHead;
+
+        int round = 1;
+        while (p.next != null) {
+
+            ListNode tmp = p.next;
+            p.next = tmp.next;
+            tmp.next = tmpDummy.next;
+
+            tmpDummy.next = tmp;
+
+            round++;
+
+            System.out.println("round: " + round);
+            if (round % k == 0 && p.next != null) {
+                tmpDummy = p;
+                p = p.next;
+                round = 1;
+                System.out.println("P: " + p.val);
+            }
+        }
+
+        return dummyHead.next;
+    }
+
     public static void main(String[] args) {
-        ListNode root = initList(0, Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        ListNode root = initList(1, Arrays.asList(2, 3, 4, 5, 6));
 
         loopList(root);
+
+        ListNode reverseNode = reverseList(root);
+
+        loopList(reverseNode);
+
+        loopList(root);
+
+        ListNode segmentNode = segmentReverseList(root, 4);
+
+        loopList(segmentNode);
     }
 }
