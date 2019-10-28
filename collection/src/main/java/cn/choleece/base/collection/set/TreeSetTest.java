e7da6e6755ae7c0e3c77317cb40bc3c6f456e390
choleece <choleece@163.com>
2019-10-28 23:36:00 +0800

    tree set

--- collection/src/main/java/cn/choleece/base/collection/set/TreeSetTest.java
+++ collection/src/main/java/cn/choleece/base/collection/set/TreeSetTest.java
@@ -1,4 +1,11 @@
 package cn.choleece.base.collection.set;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class TreeSetTest {
+
+    public static void main(String[] args) {
+        Map map = new TreeMap();
+    }
 }
