67499a7ec6b28c4acce80945905f7fa3616481f4
choleece <choleece@163.com>
2020-01-31 15:22:42 +0800

    leet code 81, 153, 167

---
+++ leetcode/src/main/java/cn/choleece/base/leetcode/solution/Solution167.java
@@ -0,0 +1,96 @@
+package cn.choleece.base.leetcode.solution;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author choleece
+ * @Description: TODO
+ * @Date 2020-01-31 14:22
+ **/
+public class Solution167 {
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int[] result = {-1, -1};
+
+        if (numbers == null || numbers.length == 0) {
+            return result;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>(numbers.length);
+
+        for (int i = 0; i < numbers.length; i++) {
+            int gap = target - numbers[i];
+
+            if (map.containsKey(gap)) {
+                return new int[] {map.get(gap) + 1, i + 1};
+            }
+
+            int res = binarySearch(numbers, gap);
+            if (res != -1 && res != i) {
+                return new int[] {i + 1, res + 1};
+            }
+
+            map.put(numbers[i], i);
+        }
+
+        return result;
+    }
+
+    public static int binarySearch(int[] numbers, int target) {
+
+        int left = 0;
+
+        int right = numbers.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (numbers[mid] == target) {
+                return mid;
+            } else if (numbers[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid -1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int[] twoSum1(int[] numbers, int target) {
+        int[] result = {-1, -1};
+
+        if (numbers == null || numbers.length == 0) {
+            return result;
+        }
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target && left != right) {
+                return new int[] {left + 1, right + 1};
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+       return result;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int[] result = twoSum(numbers, 9);
+
+        int[] result1 = twoSum1(numbers, 9);
+
+        System.out.println("result: 0 idx is : " + result[0] + ", 1 idx is : " + result[1]);
+
+        System.out.println("result1: 0 idx is : " + result1[0] + ", 1 idx is : " + result1[1]);
+    }
+}
---
+++ leetcode/src/main/java/cn/choleece/base/leetcode/solution/Solution81.java
@@ -0,0 +1,102 @@
+package cn.choleece.base.leetcode.solution;
+
+/**
+ * @author choleece
+ * @Description: TODO
+ * @Date 2020-01-31 12:58
+ **/
+public class Solution81 {
+
+    public static boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        int turnPointIdx = 0;
+        for (; turnPointIdx < nums.length - 1; turnPointIdx++) {
+            if (nums[turnPointIdx] > nums[turnPointIdx + 1]) {
+                break;
+            }
+        }
+
+        int[] nums1 = new int[turnPointIdx + 1];
+        int[] nums2 = new int[nums.length - turnPointIdx - 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i <= turnPointIdx) {
+                nums1[i] = nums[i];
+            } else {
+                nums2[i - turnPointIdx - 1] = nums[i];
+            }
+        }
+
+        return binarySearch(nums1, target) || binarySearch(nums2, target);
+    }
+
+    public static boolean binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean search1(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int midVal = nums[mid];
+
+            if (midVal == target || nums[left] == target || nums[right] == target) {
+                return true;
+            }
+
+            // 左侧有序
+            if (nums[left] < midVal) {
+                if (midVal > target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[right] > midVal) {
+                // 右侧有序
+                if (midVal < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                left++;
+                right--;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 3, 1};
+
+        System.out.println("result: " + search1(nums, 3));
+    }
+}
