3a900b7f09fa8d18ab0e81e39111a5faeeca3767
choleece <choleece@163.com>
2020-02-05 23:12:42 +0800

    leet code 465，860

---
+++ leetcode/src/main/java/cn/choleece/base/leetcode/solution/Solution455.java
@@ -0,0 +1,69 @@
+package cn.choleece.base.leetcode.solution;
+
+import java.util.Arrays;
+
+/**
+ * @author choleece
+ * @Description: TODO
+ * @Date 2020-02-05 21:21
+ **/
+public class Solution455 {
+
+    public static int findContentChildren(int[] g, int[] s) {
+        if (g == null || g.length == 0 || s == null || s.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int j = 0;
+        int count = 0;
+        for (int i = 0; i < g.length; i++) {
+            while (j < s.length) {
+                if (g[i] <= s[j]) {
+                    count++;
+                    j++;
+                    break;
+                }
+                j++;
+            }
+
+            if (j == s.length) {
+                break;
+            }
+        }
+        return count;
+    }
+
+    public static int findContentChildren1(int[] g, int[] s) {
+        if (g == null || g.length == 0 || s == null || s.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] g = {1, 2};
+        int[] s = {1, 2, 3};
+
+        System.out.println("result: " + findContentChildren(g, s));
+
+        System.out.println("result: " + findContentChildren1(g, s));
+    }
+
+}
---
+++ leetcode/src/main/java/cn/choleece/base/leetcode/solution/Solution860.java
@@ -0,0 +1,55 @@
+package cn.choleece.base.leetcode.solution;
+
+/**
+ * @author choleece
+ * @Description: TODO
+ * @Date 2020-02-05 21:46
+ **/
+public class Solution860 {
+
+    public static boolean lemonadeChange(int[] bills) {
+        if (bills == null || bills.length == 0) {
+            return true;
+        }
+
+        int fiveCount = 0;
+        int tenCount = 0;
+
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5) {
+                fiveCount++;
+            }
+
+            if (bills[i] == 10) {
+                if (fiveCount <= 0) {
+                    return false;
+                }
+                fiveCount--;
+                tenCount++;
+            }
+
+            if (bills[i] == 20) {
+
+                if (tenCount <= 0) {
+                    if (fiveCount < 3) {
+                        return false;
+                    }
+                    fiveCount -= 3;
+                } else {
+                    if (fiveCount <= 0) {
+                        return false;
+                    }
+                    tenCount--;
+                    fiveCount--;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] bills = {5, 10, 10, 5};
+        System.out.println("result: " + lemonadeChange(bills));
+    }
+}
