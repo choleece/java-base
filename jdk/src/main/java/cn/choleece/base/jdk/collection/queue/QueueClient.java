6d1f6d9ec518213a5b4422a303ddc6290658e9bd
choleece <choleece@163.com>
2020-05-13 23:32:04 +0800

    队列

---
+++ jdk/src/main/java/cn/choleece/base/jdk/collection/queue/QueueClient.java
@@ -0,0 +1,40 @@
+package cn.choleece.base.jdk.collection.queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author choleece
+ * @Description: 队列：普通队列和阻塞队列
+ * @Date 2020-05-10 18:50
+ **/
+public class QueueClient {
+
+    public static void main(String[] args) {
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue(4);
+        try {
+            blockingQueue.put("444");
+            blockingQueue.put("444");
+            blockingQueue.put("444");
+            blockingQueue.put("444");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            blockingQueue.add("444");
+        } catch (Exception e) {
+            System.out.println("队列满了...");
+            e.printStackTrace();
+        }
+
+        try {
+            String obj = blockingQueue.poll(5, TimeUnit.SECONDS);
+            System.out.println(obj);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
